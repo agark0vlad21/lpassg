c553bf09250364e852abdcd7f3130c907e679339
agark0vlad21 <72999843+agark0vlad21@users.noreply.github.com>
2023-02-14 07:35:40 +0000

    Added command-line argument parser

--- main.py
+++ main.py
@@ -1,20 +1,14 @@
 from os import urandom
+from argparse import ArgumentParser
+
+Parser = ArgumentParser(description="Simple password generator")
+Parser.add_argument("-l", "--length", help="Length of password", type=int, default=8)
+args, unknown = Parser.parse_known_args()  # Unrecognised args will be ignored
+
+passleng = 0  # Length counter for password
 
 while True:
-    try:
-        length = int(input("Length for password: "))
-        if length <= 0:
-            raise ValueError
-        break
-    except ValueError:
-        print("Invalid length")
-    except (EOFError, KeyboardInterrupt):
-        exit()
-
-passleng = 0
-
-while True:
-    if passleng == length:
+    if passleng == args.length:
         break
     try:
         char = urandom(1).decode("UTF-7")
